2c657c524e6c42c383935e313ef10cc613832dde
tylerlemke <lemke164@umn.edu>
2015-02-06 13:06:17 -0600

    Added functions to our gpaCalc.controller.js and created a gpaCalc.controller.spec.js file for our testing.

--- client/views/gpaCalc/gpaCalc.controller.js
+++ client/views/gpaCalc/gpaCalc.controller.js
@@ -6,6 +6,42 @@
 angular.module('appModule')
     .controller('gpaCtrl', function($scope){
 
+        'use strict';
+
+        console.log("main.js loaded!");
+
+        $scope.data = [];
+
+        $scope.getGPA = function(){
+            $http.get('api/pets').success(function(pets) {
+                $scope.data = pets;
+            });
+        };
+
+        $scope.getGPA();
+
+        $scope.addData = function(){
+            if($scope.gradeField.length >= 1 && $scope.creditField > 0 && $scope.creditField) {
+                $http.post('api/pets', {text: $scope.gradeField, number: $scope.creditField}).success(function(){
+                    $scope.getGPA();
+                });
+                $scope.gradeField = "";
+                $scope.creditField = 0;
+            }
+        };
+
+        $scope.removeData = function(index){
+            $http.delete('/api/pets/' + $scope.data[index]._id).success(function(){
+                $scope.getGPA();
+            });
+        };
+
+        $scope.itemsInList = function(){
+            return $scope.data.length;
+        };
+
+
+
 
 
     });
